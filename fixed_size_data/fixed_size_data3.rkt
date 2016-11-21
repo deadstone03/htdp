b785369892ca4e4b63d1b1195abe334acfc1b2dc
deadstone <deadstone03@gmail.com>
2016-11-20 17:28:37 -0800

    fix_data

---
+++ fixed_size_data/fixed_size_data3.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname fixed_size_data3) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+(define (number->square s)
+  (square s "solid" "red"))
+(define (reset s ke)
+  100)
+(big-bang 100
+          [to-draw number->square]
+          [on-tick sub1]
+          [stop-when zero?]
+          [on-key reset])
+ 
---
+++ fixed_size_data/fixed_size_data4.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname fixed_size_data4) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+(define BACKGROUND (empty-scene 100 100))
+(define DOT (circle 3 "solid" "red"))
+
+(define (main y)
+  (big-bang y
+            [on-tick sub1]
+            [stop-when zero?]
+            [to-draw place-dot-at]
+            [on-key stop]))
+(define (stop y key)
+  0)
+(define (place-dot-at y)
+  (place-image DOT 50 y BACKGROUND))
