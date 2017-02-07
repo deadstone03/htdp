6bf7be0e07f2eaf7dd155406526a07b32248050b
mato <deadstone03@gmail.com>
2017-02-06 23:50:49 -0800

    commit

--- accumulator/31 the loss of knowledge/exercise1.rkt
+++ accumulator/31 the loss of knowledge/exercise1.rkt
@@ -26,3 +26,65 @@
  
 (define (add-to-each n l)
   (map (lambda (i) (+ i n)) l))
+
+; Exercise 500
+; List-Of-Number -> Number
+; product of the list of number
+(check-expect (product (list 2 3 4)) 24)
+(define (product lon0)
+  (local
+    (
+     ; List-Of-Number Number -> Number
+     ; proudct of the list.
+     ; accumulator is the product of the numbers before lon.
+     (define (product/a lon a)
+       (cond
+         [(empty? lon) a]
+         [else (product/a (rest lon) (* (first lon) a))])))
+    (product/a lon0 1)))
+
+; Exercise 501
+; List -> Number
+; length of the list
+(check-expect (how-many (list 1 2 3 4)) 4)
+(define (how-many lon)
+  (local
+    (
+     ; List Number -> Number
+     ; lenght of the list
+     ; accumulator is the length before lon
+     (define (how-many/a lon l)
+       (cond
+         [(empty? lon) l]
+         [else (how-many/a (rest lon) (+ 1 l))])))
+    (how-many/a lon 0)))
+
+; Exercise 503
+; [NEList-of 1String] -> [NEList-of 1String]
+; create a palindrome from s0
+(check-expect
+  (mirror (explode "abc")) (explode "abcba"))
+(define (mirror s0)
+  (local
+    (
+     ; [NEList-of 1String] List-of-String -> [NEList-of 1String]
+     ; palindrome of s
+     ; accumulator is the palindrome for the 1String before s
+     (define (mirror/a s a)
+       (cond
+         [(empty? (rest s)) (cons (first s) a)]
+         [else (cons (first s) (mirror/a (rest s) (cons (first s) a)))])))
+    (mirror/a s0 '())))
+
+; Exercise 507
+; [x -> y] [List-of x] -> [List-of Y]
+; mapping every x in the list to y
+(check-expect (mymap add1 (list 1 2 3 4)) (list 2 3 4 5))
+(define (mymap f lox0)
+  (local
+    (
+     (define (mymap/a lox a)
+       (cond
+         [(empty? lox) a]
+         [else (mymap/a (rest lox) (cons (f (first lox)) a))])))
+    (mymap/a (reverse lox0) '())))
